effad37057ffbeae3bfc7f2b9dae8488516dff60
borovskayana <borovskayana7@gmail.com>
2023-08-10 01:53:33 +0300

    Add

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import App from './App';
 
 import reportWebVitals from './reportWebVitals';
 
+app.use(express.static(__dirname));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
